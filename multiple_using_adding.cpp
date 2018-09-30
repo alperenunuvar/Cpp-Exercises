7bbd0f6b23449ee5f1ba7767322a4e178fb0ba40
Alp <alperen_unuvar@hotmail.com>
2018-09-30 18:04:45 +0300

    Add files via upload

---
+++ multiple_using_adding.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+//recursion
+int multiplication_r(int num, int i)
+{
+	if(i==0)
+	{
+		return 0;
+	}
+	return num+multiplication_r(num,i-1);
+}
+
+//iterative
+int multiplication(int num, int i)
+{
+	// Variables
+	int a;
+	int sum;
+
+	sum=0;
+	for(a=0;a<i;a++)
+	{
+		sum+=num;
+	}
+	return sum;
+}
+
+int main()
+{
+	// Variables
+	int number1;
+	int number2;
+	int c;
+
+	cout << "Enter first integer to multiplication = ";
+	cin >> number1;
+	cout << "Enter second integer to multiplication = ";
+	cin >> number2;
+	//iterative
+	//cout << number1 << " x " << number2 << " = " << multiplication(number1, number2) << endl;
+	//recursion
+	cout << number1 << " x " << number2 << " = " << multiplication_r(number1, number2) << endl;
+	return 0;
+}

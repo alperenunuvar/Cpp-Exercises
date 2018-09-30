eb8b1bc3c81513134479f3644ec67c0c28b2abd4
Alp <alperen_unuvar@hotmail.com>
2018-09-30 23:47:34 +0300

    Add files via upload

---
+++ array_works.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// Arrays
+	int numbers[3] = {10,9,8};
+
+	// Variables
+	int i;
+
+	cout << "numbers[1] = " << numbers[1] << endl;
+	cout << "Numbers = ";
+	for(i=0;i<3;i++)
+	{
+		cout << numbers[i] << " ";
+	}
+	cout << "\nReverse Numbers = ";
+	for(i=2;i>=0;i--)
+	{
+		cout << numbers[i] << " ";
+	}
+	cout << endl;
+	for(i=0;i<3;i++)
+	{
+		cout << "Numbers[" << i << "]" << " = " << numbers[i] << endl;
+	}
+	for(i=0;i<3;i++)
+	{
+		cout << "Enter " << i+1 << ". numbers = ";
+		cin >> numbers[i];
+	}
+	for(i=0;i<3;i++)
+	{
+		cout << numbers[i] << " ";
+	}
+	cout << "\nRandom Numbers (0-100) = ";
+	srand(time(NULL));
+	for(i=0;i<3;i++)
+	{
+		numbers[i] = rand()%101;
+		cout << numbers[i] << " ";
+	}
+	cout << endl;
+	return 0;
+}

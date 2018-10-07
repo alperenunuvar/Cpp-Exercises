1bc9a1d481df8010c9952f2dc839f486610b167b
Alp <alperen_unuvar@hotmail.com>
2018-10-07 05:10:03 +0300

    Add files via upload

---
+++ prime_mersenne_numbers.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+
+using namespace std;
+
+int calculatePow(int num, int exponent)
+{
+	// Variables
+	int i;
+	int total;
+
+	total=1;
+	for(i=0;i<exponent;i++)
+	{
+		total=total*num;
+	}
+	return total;
+}
+
+int calculateMersenneNumber(int number)
+{	
+	return calculatePow(2,number)-1;
+}
+
+bool isPrime(int num)
+{
+	// Variables
+	int i;
+
+	if(num<2)
+	{
+		return false;
+	}
+	for(i=2;i<num;i++)
+	{
+		if(num%i==0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+int main()
+{	
+	// Variables
+	int arraySize;
+	int i;
+	int j;
+	int a;
+
+	cout << "Input an array size = ";
+	cin >> arraySize;
+	if(arraySize>0)
+	{
+		// Arrays
+		int mersennePrimeNumbers[arraySize];
+		int mersenneNumbers[arraySize];
+
+		j=0;
+		for(i=1;i<=arraySize;i++)
+		{
+			mersenneNumbers[j] = calculateMersenneNumber(i);
+			j++;	
+		}
+		cout << endl;
+		cout << "First " << arraySize << " Mersenne Numbers : ";
+		for(i=0;i<arraySize;i++)
+		{
+			cout << mersenneNumbers[i] << " ";
+		}
+		cout << endl;	
+		a=0;
+		for(i=0;i<arraySize;i++)
+		{
+			if(isPrime(mersenneNumbers[i])==true)
+			{			
+				mersennePrimeNumbers[a]=mersenneNumbers[i];
+				a++;			
+			}
+		}
+		cout << "\nMersenne Prime Numbers = ";
+		for(i=0;i<a;i++)
+		{
+			cout << mersennePrimeNumbers[i] << " ";
+		}
+		cout << endl;
+	}		
+	else
+	{
+		cout << "Array size must be positive integer." << endl;
+	}
+	return 0;
+}

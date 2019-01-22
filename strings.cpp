7d400154949c950e99dc973f599438c4b97ced05
Alp <alperen_unuvar@hotmail.com>
2019-01-22 03:58:16 +0300

    Add files via upload

---
+++ strings.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// Strings
+	char *name = "Eric";
+	char *surname = "Paul";
+	char other[5] = "Exam";
+
+	cout << "Full Name = " << name << " " << surname << endl;
+	cout << other[2] << endl;
+	return 0;
+}
